5d3254b9cbae8e2358bdd49c957027671496e4e9
ZdenekZaruba <zdenek.zaruba98@icloud.com>
2021-11-01 13:30:15 +0100

    school

--- PHP/minefield.php
+++ PHP/minefield.php
@@ -1,10 +1,22 @@
 <?php
-define("SIRKA", 20);
-define("VYSKA", 5);
+define("SIRKA", 3);
+define("VYSKA", 3);
 $pole = init();
-show($pole);
+generujMiny($pole, 8);
+ukaz($pole);
 
-function show(array $p): void {
+function generujMiny($p, $count){
+    for($i = 1; $i <= $count; $i++){
+        do {
+            $x = rand(0, SIRKA - 1);
+            $y = rand(0, VYSKA - 1);
+        } while ($p[$x][$y] !== 0);
+        $p[$x][$y] = "M";
+    }
+    return $p;
+}
+
+function ukaz(array $p): void {
     for($y = 0 ; $y < VYSKA; $y++){
         for($x = 0 ; $x < SIRKA; $x++){
         echo $p[$x][$y];
