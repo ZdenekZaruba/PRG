6ffcb5d3a16660849de8a9729aef02c212a275fd
ZdenekZaruba <56403536+ZdenekZaruba@users.noreply.github.com>
2022-04-11 14:05:40 +0200

    test

---
+++ PHP/Database/database.php
@@ -0,0 +1,67 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "school";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+echo "<br />";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+echo "<br />";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+echo "<br />";
+
+// Create table
+$sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+echo "<br />";
+
+// Fill into table
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Michal', 'Bubilek', 'michal.bubilek@skolavdf.cz')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
